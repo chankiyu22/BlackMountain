085e6efcad3e70af8505ac3992c19924ec5460c7
Argo15 <wcrane15@gmail.com>
2013-04-15 05:36:34 -0400

    Websockets: Mentions and Interactions

--- public/javascript/connect.js
+++ public/javascript/connect.js
@@ -15,5 +15,27 @@ var Connect = {
 		   	'</div></li>';
 		   	$('#interaction-list').prepend(interaction_html);
 	  	});
+
+	  	this.socket.on('+Mention', function (data) {
+	  		var interaction_html = '<li><div class="tweet">' +
+		   		'<a class="owner" href="/' + data.tweet.userdata.username + '">' +
+		   			'<b>' + data.tweet.userdata.fullname + '</b>' +
+		   		'</a>' + ' mentioned you' +
+		   	'</div></li>';
+		   	$('#interaction-list').prepend(interaction_html);
+
+		   	var tweet_html = '<div class="tweet">' +
+		   		'<a href="/' + data.tweet.owner + '">' +
+		   			'<div class="owner">' +
+		   				data.tweet.userdata.fullname + 
+		   				'<small> @' + 
+		   				data.tweet.owner +
+		   				'</small>' +
+		   			'</div>' +
+		   		'</a>' +
+		   		data.tweet.html +
+		   	'</div>';
+		   	$('#tweet-list').prepend(tweet_html);
+	  	});
 	}
 };
