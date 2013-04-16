3b953de7f938787e9da69bf92b48fee9f6713ef4
Argo15 <wcrane15@gmail.com>
2013-04-15 20:12:44 -0400

    Added a search feature.

---
+++ routes/search.js
@@ -0,0 +1,14 @@
+var user = require('../lib/user');
+var tweets = require('../lib/tweets');
+var util = require('../lib/util');
+
+// ## GET /search?searchfor='string'<br>
+// Gets search page containing all the tweets containing the searchfor string
+exports.search = function(req, res){
+	var searchstring = req.query.searchfor;
+	var tweet_array = tweets.getTweetsContaining(searchstring);
+	util.initTweets(tweet_array);
+  	res.render('search', {user: user.getUser(req.session.username),
+  						tweets: tweet_array,
+  						timeline_header: 'Search Results For ' + searchstring});
+};
