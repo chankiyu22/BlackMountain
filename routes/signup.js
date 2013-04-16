03143d14a9ff5b9c21a52701a0ba2b8d69a5c58f
GideonZhi <aeongen@gmail.com>
2013-04-16 19:06:50 -0300

    Update signup.js

--- routes/signup.js
+++ routes/signup.js
@@ -4,7 +4,7 @@ var users = require('../lib/user');
 // Display information for signup page
 exports.index = function(req, res){
   res.render('signup', {first: 'Twitter',
-  						name: (req.body.name ? req.body.name : ""),
+    					name: (req.body.name ? req.body.name : ""),
   						email: (req.body.email ? req.body.email : ""),
               error: req.session.signup_error,
   						password: (req.body.password ? req.body.password : ""),
@@ -27,7 +27,7 @@ exports.createUser = function(req, res) {
   console.log("signup " + username + " " + pwd);
   users.signup(fullname, email, username, pwd, function(error, userobj){
     // If all info is present, signup is successful and redirects to '/'
-    if (userobj !== undefined)
+    if (error == undefined)
     {
 
       req.session.fullname = userobj.fullname;
@@ -42,9 +42,11 @@ exports.createUser = function(req, res) {
     else
     {
       req.session.signup_error = error;
+      signup_error=error;
       req.session.login = false;
-      res.redirect('/signup');
+      res.send('error');
+      //res.redirect('/signup');
     } 
   });
+  //res.send('success');
 }
-
